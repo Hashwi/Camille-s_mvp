4b1fcbfa2ac4db2a618f5ca71b8a9b6169386728
Camille Richet <camille.l.a.richet@gmail.com>
2023-05-23 16:03:13 +0100

    questionnaire get 1 set question.answers tested

--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const db = require("../model/helper");
 
+/* SELECT * FROM table1 
+  INNER JOIN table2
+  ON table1.id = table2.id; */
+
+
 // GET all the questionnaire.
 router.get('/', function(req, res, next) {
   db("SELECT * from quiz INNER JOIN answers ON quiz.id = answers.question_ID;")
@@ -12,16 +17,12 @@ router.get('/', function(req, res, next) {
 });
 
 
-/* SELECT * FROM table1 
-  INNER JOIN table2
-  ON table1.id = table2.id; */
-
-// GET one question with matchin answers
+// GET one question with all matching answers
 router.get("/:question_id", async function(req, res) {
   const { question_id } = req.params;
   try {
     const results = await db(
-      `SELECT * FROM quiz WHERE id = ${question_id};`
+      `SELECT * FROM quiz INNER JOIN answers ON quiz.id = answers.question_ID WHERE quiz.id = ${question_id};`
     );
     res.send(results.data);
   } catch (error) {
