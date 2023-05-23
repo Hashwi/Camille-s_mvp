96009de05bca57fb82fcf966374a90197863972c
Camille Richet <camille.l.a.richet@gmail.com>
2023-05-23 14:45:25 +0100

    fix admin/user view toggle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
+import AdminView from './components/AdminView';
+import UserView from './components/UserView';
 
 function App() {
   const [isAdmin, setIsAdmin] = useState(false);
@@ -11,11 +13,11 @@ function App() {
   };
 
   return (
-      <div className='appContainer'>
+      <div className="appContainer">
         <h1>Ingredients Finder</h1>
         <aside>
-          <button className="adminButton" style={styles.adminButton} onClick={() => handleChangeView(true)}>Admin</button>        
-          <button className="userButton" style={styles.userButton} onClick={() => handleChangeView(false)}>User</button>
+          <button className="adminButton" onClick={() => handleChangeView(true)}>Admin</button>        
+          <button className="userButton" onClick={() => handleChangeView(false)}>User</button>
         </aside>
         {isAdmin? (
         <AdminView />
