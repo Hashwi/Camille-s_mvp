56b2131ff483820d22befb657459fced142f094a
Camille Richet <camille.l.a.richet@gmail.com>
2023-05-23 11:01:22 +0100

    DB schema correction

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', indexRouter);
-app.use('/questions', questionsRouter);
+app.use('/api/index', indexRouter);
+app.use('/api/questions', questionsRouter);
 
 module.exports = app;
