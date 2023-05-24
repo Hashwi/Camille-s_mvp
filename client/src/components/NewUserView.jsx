8bc262649c5544207b7dea82454bc521b547861f
Camille Richet <camille.l.a.richet@gmail.com>
2023-05-24 14:29:00 +0100

    added handleResponse function

--- client/src/components/NewUserView.jsx
+++ client/src/components/NewUserView.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 
 function UserView() {
   const [quiz, setQuiz] = useState({ question: "", answers: [] });
+  const [finish, setFinish] = useState(false);
+  const [selectedValues, setSelectedValues] = useState([]);
 
   useEffect(() => {
     showQuestionnaire(1);
@@ -18,28 +20,43 @@ function UserView() {
     });
   };
 
+  const handleResponse = (value) => {
+    if (selectedValues.includes(value)) {
+      setSelectedValues(selectedValues.filter((v) => v !== value));
+    } else {
+      setSelectedValues([...selectedValues, value]);
+    };
+  }
 
   const handleSubmit = event => {
     event.preventDefault();
     // addAnswer();
     showQuestionnaire((quiz.id)+1);
   };
-  const { question, answers } = quiz;
+  const { id, question, answers } = quiz;
+
+  const finishHandler = () => {
+    if (currentQuestion === data.length - 1) {
+      setFinish(true);
+      //showResult()
+    }
+  };
+
 
   return (
     <div>
         <h2>New here?</h2>
         <h3>Skin assessment</h3>
-        <form onSubmit={handleSubmit}>
-        <h4>{question}</h4>
-          <ul>
-            {answers.map((answer, index) => (
-                <li className="quiz" key={index}>
-                  {answer}
-                </li>
-            ))}
-          </ul>
-          <button type="submit">Submit</button>
+        <form>
+          <div>
+            <h4>{id}. {question}</h4>
+                {answers.map((answer, index) => (
+                    <button className={selectedValues.includes(answer) ? "selected" : ""} key={index} onClick={handleResponse(answer)}>
+                      {answer}
+                    </button>
+                ))}
+            </div>
+          <button type="submit" onClick={handleSubmit}>Submit</button>
         </form>
     </div>
   );
