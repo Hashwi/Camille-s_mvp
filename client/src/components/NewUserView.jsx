90a6c83e383669c813a4c752ee6f2ce1b0e24259
Camille Richet <camille.l.a.richet@gmail.com>
2023-05-24 16:33:33 +0100

    attempt to fix answers toggle

--- client/src/components/NewUserView.jsx
+++ client/src/components/NewUserView.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 
 function UserView() {
   const [quiz, setQuiz] = useState({ question: "", answers: [] });
-  const [finish, setFinish] = useState(false);
   const [selectedValues, setSelectedValues] = useState([]);
+  const [isSelected, setIsSelected] = useState([]);
 
   useEffect(() => {
     showQuestionnaire(1);
@@ -14,50 +14,77 @@ function UserView() {
     .then(response => response.json())
     .then(data => {
       setQuiz(data);
+      setIsSelected(Array(data.answers.length).fill(false));
     })
     . catch(error => {
     console.log(error)
     });
   };
 
-  const handleResponse = (value) => {
-    if (selectedValues.includes(value)) {
-      setSelectedValues(selectedValues.filter((v) => v !== value));
-    } else {
-      setSelectedValues([...selectedValues, value]);
-    };
+  const handleToggle = (index) => {
+    setIsSelected((prevState) => {
+      const updatedStates = [...prevState];
+      updatedStates[index] = !updatedStates[index];
+      return updatedStates;
+    });
   }
 
   const handleSubmit = event => {
     event.preventDefault();
-    // addAnswer();
-    showQuestionnaire((quiz.id)+1);
-  };
-  const { id, question, answers } = quiz;
 
-  const finishHandler = () => {
-    if (currentQuestion === data.length - 1) {
-      setFinish(true);
-      //showResult()
+    if (quiz.id === totalQuestions) {
+      setSelectedValues([])
+      setIsSelected(Array(quiz.answers.length).fill(false))
+      showQuestionnaire(1)
+    }
+    
+    const selected = [];
+    isSelected.forEach((state, index) => {
+      if (state) {
+        selected.push(quiz.answers[index]);
+      }
+    });
+
+    if (selected.length > 0) {
+      setIsSelected([...selectedValues, ...selected]);
+    }
+
+    if (quiz.id < totalQuestions) {
+      showQuestionnaire((quiz.id)+1);
     }
   };
 
+  const { id, question, answers } = quiz;
+  const totalQuestions = 5;
 
   return (
     <div>
         <h2>New here?</h2>
         <h3>Skin assessment</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
-            <h4>{id}. {question}</h4>
-                {answers.map((answer, index) => (
-                    <button className={selectedValues.includes(answer) ? "selected" : ""} key={index} onClick={handleResponse(answer)}>
-                      {answer}
-                    </button>
-                ))}
-            </div>
-          <button type="submit" onClick={handleSubmit}>Submit</button>
+            <h4>
+              {id}. {question}
+            </h4>
+            <ul>
+              {answers.map((answer, index) => (
+                <li key={answers[index]}>
+                  <button 
+                      className={isSelected? "selected" : ""} 
+                      onClick={()=>handleToggle(index)}
+                  >{answer}</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <button type="submit">Submit</button>
         </form>
+        <div>
+          <h4>Your answers are:</h4>
+          {selectedValues.map((value, index) => (
+                <p key={index}>{value}</p>
+              ))}
+        </div>
     </div>
   );
 }
